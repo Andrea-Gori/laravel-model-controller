71c325ae54f52585354cc871c46ba3c3c635eca8
Andrea-Gori <andrea.fr.gori@gmail.com>
2021-05-18 18:10:03 +0200

    completo?

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,22 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
+        <main>
+            <div class="container">
         @foreach($movies as $boh)
-            <p>{{ $boh->title }}</p>
-        @endforeach
+                <div class="card">
+                    <img src="https://teeserdev.s3.amazonaws.com/snap_TommyVI_1473103882003.png" alt="">
+                    <p class="title">{{ $boh->title }}</p>
+                    <p class="original-title">{{ $boh->original_title }}</p>
+                    <p class="nationality">{{ $boh->nationality }}</p>
+                    <p class="vote">{{ $boh->vote }}</p>
+                </div>
+            @endforeach
+            </div>
+        </main>
     </body>
 </html>
